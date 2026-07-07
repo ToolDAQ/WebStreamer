240e47535f581c6c16bce13abe17e060b6e20f38
brichards64 <benjamin.richards@warwick.ac.uk>
2026-07-07 14:36:59 +0000

    forgot header

---
+++ UserTools/ServiceDiscoverySource/ServiceDiscoverySource.h
@@ -0,0 +1,58 @@
+#ifndef ServiceDiscoverySource_H
+#define ServiceDiscoverySource_H
+
+#include <string>
+#include <iostream>
+
+#include "Tool.h"
+#include "DataModel.h"
+
+/**
+ * \struct ServiceDiscoverySource_args_args
+ *
+ * This is a struct to place data you want your thread to access or exchange with it. The idea is the datainside is only used by the threa\d and so will be thread safe
+ *
+ * $Author: B.Richards $
+ * $Date: 2019/05/28 10:44:00 $
+ */
+
+struct ServiceDiscoverySource_args:Thread_args{
+
+  ServiceDiscoverySource_args();
+  ~ServiceDiscoverySource_args();
+  DataModel* m_data = 0;
+  DAQUtilities* m_utils = 0;
+  std::vector<Store> services;
+  std::string message ="";
+};
+
+/**
+ * \class ServiceDiscoverySource
+ *
+ * This is a template for a Tool that produces a single thread that can be assigned a function seperate to the main thread. Please fill out the descripton and author information.
+*
+* $Author: B.Richards $
+* $Date: 2019/05/28 10:44:00 $
+*/
+
+class ServiceDiscoverySource: public Tool {
+
+
+ public:
+
+  ServiceDiscoverySource(); ///< Simple constructor
+  bool Initialise(std::string configfile,DataModel &data); ///< Initialise Function for setting up Tool resorces. @param configfile The path and name of the dynamic configuration file to read in. @param data A reference to the transient data class used to pass information between Tools.
+  bool Execute(); ///< Executre function used to perform Tool perpose. 
+  bool Finalise(); ///< Finalise funciton used to clean up resorces.
+
+
+ private:
+
+  static void Thread(Thread_args* arg); ///< Function to be run by the thread in a loop. Make sure not to block in it
+  Utilities* m_util;  ///< Pointer to utilities class to help with threading
+  ServiceDiscoverySource_args* args; ///< thread args (also holds pointer to the thread)
+
+};
+
+
+#endif
